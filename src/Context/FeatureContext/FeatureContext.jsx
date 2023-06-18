3e6ec648afd4bfe9affde08a6599f44edda66a2b
Shreyansh Tiwari <shreyansh.tiwari@applyboard.com>
2023-06-19 02:05:04 +0530

    Update FeatureContext.jsx

--- src/Context/FeatureContext/FeatureContext.jsx
+++ src/Context/FeatureContext/FeatureContext.jsx
@@ -1,178 +1,183 @@
-import { createContext, useContext,useState} from "react"
+import { createContext, useContext, useState } from "react";
 import { authContext } from "../authContext/authContext";
 
 export const featureContext = createContext();
 
-export default function FeatureProvider({children}){
+export default function FeatureProvider({ children }) {
+  const { authDispatch, userPostList, authState } = useContext(authContext);
 
-    const {authDispatch,userPostList,authState} = useContext(authContext);
+  const [trending, setTrending] = useState(false);
 
-    const [trending,setTrending] = useState(false);
+  const addPost = async (postContent) => {
+    try {
+      console.log(postContent);
+      const response = await fetch("/api/posts", {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: JSON.stringify({ postData: { content: postContent } }),
+      });
 
-    const addPost = async(postContent) =>{
-        try{
-            console.log(postContent);
-            const response = await fetch('/api/posts',{
-                method:'POST',
-                headers: {authorization: localStorage.getItem('encodedToken')},
-                body:
-                    JSON.stringify({postData: {content: postContent}})   
-            })
-
-            const {posts} = await response.json();
-            authDispatch({type:'allPostList',payload:posts});
-            authDispatch({type:'postList',payload:posts});
-            if(trending){
-                const sortByLikes =posts.sort((a,b) => b.likes.likeCount - a.likes.likeCount );
-                authDispatch({type:'postList',payload:sortByLikes});
-            }
-            authDispatch({type:'postContent', payload:''})
-
-        }catch(error){
-            console.log('error')
-        }
+      const { posts } = await response.json();
+      authDispatch({ type: "allPostList", payload: posts });
+      authDispatch({ type: "postList", payload: posts });
+      if (trending) {
+        const sortByLikes = posts.sort(
+          (a, b) => b.likes.likeCount - a.likes.likeCount
+        );
+        authDispatch({ type: "postList", payload: sortByLikes });
+      }
+      authDispatch({ type: "postContent", payload: "" });
+    } catch (error) {
+      console.log("error");
     }
+  };
 
-    const editPost = async(postId,postContent) =>{
-        try{
-            console.log(postId,postContent);
-            
-            const response = await fetch(`/api/posts/edit/${postId}`,{
-                method:'POST',
-                headers: {authorization: localStorage.getItem('encodedToken')},
-                body: JSON.stringify({postData: {content: postContent}})
-            });
+  const editPost = async (postId, postContent) => {
+    try {
+      console.log(postId, postContent);
 
-            const {posts} = await response.json();
+      const response = await fetch(`/api/posts/edit/${postId}`, {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: JSON.stringify({ postData: { content: postContent } }),
+      });
 
-            authDispatch({type:'allPostList',payload:posts});
-            authDispatch({type:'postList',payload:posts});
-            if(trending){
-                const sortByLikes =posts.sort((a,b) => b.likes.likeCount - a.likes.likeCount );
-                authDispatch({type:'postList',payload:sortByLikes});
-            }
-            authDispatch({type:'postContent', payload:''})
+      const { posts } = await response.json();
 
-        }catch(error){
-            console.log(error)
-        }
+      authDispatch({ type: "allPostList", payload: posts });
+      authDispatch({ type: "postList", payload: posts });
+      if (trending) {
+        const sortByLikes = posts.sort(
+          (a, b) => b.likes.likeCount - a.likes.likeCount
+        );
+        authDispatch({ type: "postList", payload: sortByLikes });
+      }
+      authDispatch({ type: "postContent", payload: "" });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const likePost = async(postId) =>{
-        try{
-            console.log(postId)
-            const response = await fetch(`/api/posts/like/${postId}`,{
-                method: 'POST',
-                headers: {authorization: localStorage.getItem('encodedToken')},
-                body:{},
-            });
-            const {posts} = await response.json();
-            authDispatch({type:'allPostList',payload:posts});
-            userPostList();
-
-        }catch(error){
-            console.log(error)
-        }
+  const likePost = async (postId) => {
+    try {
+      console.log(postId);
+      const response = await fetch(`/api/posts/like/${postId}`, {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: {},
+      });
+      const { posts } = await response.json();
+      authDispatch({ type: "allPostList", payload: posts });
+      userPostList();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const dislikePost = async(postId) =>{
-            try{
-                const response = await fetch(`/api/posts/dislike/${postId}`,{
-                    method:'POST',
-                    headers: {authorization: localStorage.getItem('encodedToken')},
-                    body:{},
-                })
-                const {posts} = await response.json();
-                authDispatch({type:'allPostList',payload:posts});
-                userPostList();
-            }catch(error){
-                console.log(error)
-            }
+  const dislikePost = async (postId) => {
+    try {
+      const response = await fetch(`/api/posts/dislike/${postId}`, {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: {},
+      });
+      const { posts } = await response.json();
+      authDispatch({ type: "allPostList", payload: posts });
+      userPostList();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-
-    const addBookmark = async(postId) =>{
-        try{
-            console.log(postId)
-            const response = await fetch(`/api/users/bookmark/${postId}`,{
-                method:'POST',
-                headers:{authorization: localStorage.getItem('encodedToken')},
-                body:{}
-            })
-        const {bookmarks} = await response.json();
-        console.log(bookmarks)
-        authDispatch({type:'bookmarks', payload: bookmarks })
-
-        }catch(error){
-            console.log(error)
-        }
+  const addBookmark = async (postId) => {
+    try {
+      console.log(postId);
+      const response = await fetch(`/api/users/bookmark/${postId}`, {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: {},
+      });
+      const { bookmarks } = await response.json();
+      console.log(bookmarks);
+      authDispatch({ type: "bookmarks", payload: bookmarks });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const removeBookmark = async(postId) =>{
-        try{
-            const response = await fetch(`/api/users/remove-bookmark/${postId}`,{
-                method: 'POST',
-                headers:{authorization: localStorage.getItem('encodedToken')},
-                body:{}
-            })
-            const {bookmarks}  = await response.json();
-            authDispatch({type:'bookmarks', payload: bookmarks })
-        }catch(error){
-            console.log(error)
-        }
+  const removeBookmark = async (postId) => {
+    try {
+      const response = await fetch(`/api/users/remove-bookmark/${postId}`, {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: {},
+      });
+      const { bookmarks } = await response.json();
+      authDispatch({ type: "bookmarks", payload: bookmarks });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const deletePost = async(postId) =>{
-        try{
-            console.log(postId)
-            const response = await fetch(`/api/posts/${postId}`,{
-                method:'DELETE',
-                headers: {authorization: localStorage.getItem('encodedToken')},  
-            })
+  const deletePost = async (postId) => {
+    try {
+      console.log(postId);
+      const response = await fetch(`/api/posts/${postId}`, {
+        method: "DELETE",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+      });
 
-            const {posts} = await response.json();
-            console.log(posts)
-            authDispatch({type:'allPostList', payload:posts})
-            userPostList();
-        }catch(error){
-            console.log(error)
-        }
+      const { posts } = await response.json();
+      console.log(posts);
+      authDispatch({ type: "allPostList", payload: posts });
+      userPostList();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const follow = async(followUserId) =>{
-        try{
-            const response = await fetch(`/api/users/unfollow/${followUserId}`,{
-                method:'POST',
-                headers: {authorization: localStorage.getItem('encodedToken')},
-                body:{}
-            })
-            const {user,followUser} = await response.json();
-
-        }catch(error){
-            console.log(error);
-        }
+  const follow = async (followUserId) => {
+    try {
+      const response = await fetch(`/api/users/unfollow/${followUserId}`, {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: {},
+      });
+      const { user, followUser } = await response.json();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const unFollow = async(followUserId) =>{
-        try{
-            const response = await fetch(`/api/users/follow/${followUserId}`,{
-                method:'POST',
-                headers: {authorization: localStorage.getItem('encodedToken')},
-                body:{}
-            })
-            const {user,followUser} = await response.json();
-            
-        }catch(error){
-            console.log(error);
-        }
+  const unFollow = async (followUserId) => {
+    try {
+      const response = await fetch(`/api/users/follow/${followUserId}`, {
+        method: "POST",
+        headers: { authorization: localStorage.getItem("encodedToken") },
+        body: {},
+      });
+      const { user, followUser } = await response.json();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return(
-        <div>
-            <featureContext.Provider value={{likePost,dislikePost,addBookmark,removeBookmark,addPost,deletePost,setTrending,editPost}}>
-              {children}
-            </featureContext.Provider>
-           
-        </div>
-    )
+  return (
+    <div>
+      <featureContext.Provider
+        value={{
+          likePost,
+          dislikePost,
+          addBookmark,
+          removeBookmark,
+          addPost,
+          deletePost,
+          setTrending,
+          editPost,
+        }}
+      >
+        {children}
+      </featureContext.Provider>
+    </div>
+  );
 }
